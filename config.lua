1b0622be0a85106c2c7c9270f57c21b0dbb092e6
jezynapi <jezynapi@gmail.com>
2016-07-29 12:14:31 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+-- pins configuration
+pin25Dust  = 1
+pin10Dust  = 2
+
+-- time
+mainLoopTime    = 120 * 1000	-- milisekunds
+measurementTime =  90 * 1000000	-- mainLoopTime *500		-- microsekunds
+
+--measurement range, selected experimentally
+minPoint    = 5
+middlePoint = 70
+maxPoint    = 300
+
